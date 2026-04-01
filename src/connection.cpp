b8eb5ab223c3322814622c2b7665806d08f68787
moriar1 <reflowcion@tuta.io>
2026-04-01 14:49:37 +0500

    feat(HTTP): add recieving headers

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,29 +1,62 @@
 #include "connection.hpp"
+#include "customlogger.hpp"
 #include "socket.hpp"
 #include <array>
+#include <cerrno>
+#include <cstddef>
+#include <cstring>
 #include <string>
 #include <sys/socket.h>
 
 static constexpr size_t MAXDATASIZE = 4096;
+static constexpr time_t TIMEOUT = 10;
 
 void handle_client(Socket sock, std::string ip) {
-  long numbytes = 0;
+  // Set timeout for connection
+  const struct timeval time = {TIMEOUT, 0};
+  if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time)) != 0) {
+    LOG_ERRNO("failed set rcv timout");
+  }
+  if (setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &time, sizeof(time)) != 0) {
+    LOG_ERRNO("failed set snd timout");
+  }
+
+  // Recieve requests (HTTP headers)
+  ssize_t nbytes = 0;
+  size_t total_nbytes = 0;
   std::array<char, MAXDATASIZE> buf{};
   while (true) {
-    numbytes = recv(sock, buf.data(), buf.size(), 0);
-    if (numbytes == -1) {
-      LOG_ERRNO("recv");
-      break;
+    size_t spaceleft = buf.size() - total_nbytes;
+    nbytes = recv(sock, &buf[total_nbytes], spaceleft, 0);
+
+    if (nbytes < 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      if (errno == EAGAIN) {
+        LOG_INFO(ip, " timeout");
+      } else {
+        LOG_ERRNO("recv");
+      }
+      return;
     }
-    if (numbytes == 0) {
+
+    if (nbytes == 0) {
       LOG_INFO(ip, " disconnected");
       break;
     }
-    if (send(sock, buf.data(), static_cast<size_t>(numbytes), 0) == -1) {
-      LOG_ERRNO("send");
-    } else {
-      LOG_INFO("echoed to ", ip);
+
+    total_nbytes += static_cast<size_t>(nbytes);
+    if (std::string_view(buf.data(), total_nbytes).find("\r\n\r\n") !=
+        std::string::npos) { // found end of headers
+      break;
+    }
+
+    // Too long headers => 431
+    if (total_nbytes >= buf.size()) {
+      // TODO: send_431(sock);
+      break;
     }
   }
-  // TODO: handle_http_request(sock, buf); or write HttpHandler class
+  std::cout << buf.data();
 }
