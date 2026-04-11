9989384d91a6a96b220453e1d5cc766066e440cf
moriar1 <reflowcion@tuta.io>
2026-04-11 15:44:42 +0500

    feat(HTTP): add basic sending index.html file

--- src/connection.cpp
+++ src/connection.cpp
@@ -3,13 +3,40 @@
 #include "uniquefd.hpp"
 #include <array>
 #include <cerrno>
-#include <cstddef>
-#include <cstring>
+#include <cstdio>
+#include <exception>
+#include <fcntl.h>
+#include <sstream>
 #include <string>
+#include <string_view>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <system_error>
+
+#ifdef __linux__
+#include <sys/sendfile.h>
+#endif
+
+using HttpStatus = unsigned;
 
 static constexpr size_t MAXDATASIZE = 4096;
 static constexpr time_t TIMEOUT = 10;
+
+static HttpStatus handle_http_request(Socket &sock, std::string_view request);
+
+static void send_all(Socket &sock, std::string_view msg) {
+  ssize_t n = 0;
+
+  while (!msg.empty()) {
+    if ((n = send(sock, msg.data(), msg.size(), 0)) == -1) {
+      if (errno == EINTR) {
+        continue;
+      }
+      throw std::system_error(errno, std::system_category(), "send");
+    }
+    msg.remove_prefix(static_cast<size_t>(n));
+  }
+}
 
 void handle_client(Socket sock, std::string ip) {
   // Set timeout for connection
@@ -24,7 +51,7 @@ void handle_client(Socket sock, std::string ip) {
   // Recieve requests (HTTP headers)
   ssize_t nbytes = 0;
   size_t total_nbytes = 0;
-  std::array<char, MAXDATASIZE> buf{};
+  std::array<char, MAXDATASIZE> buf;
   while (true) {
     size_t spaceleft = buf.size() - total_nbytes;
     nbytes = recv(sock, &buf[total_nbytes], spaceleft, 0);
@@ -42,7 +69,7 @@ void handle_client(Socket sock, std::string ip) {
     }
 
     if (nbytes == 0) {
-      LOG_INFO(ip, " disconnected");
+      LOG_INFO(ip, " failed get request headers: client disconnected");
       break;
     }
 
@@ -58,5 +85,88 @@ void handle_client(Socket sock, std::string ip) {
       break;
     }
   }
-  std::cout << buf.data();
+  std::string_view request(buf.data(), total_nbytes);
+
+  try {
+    // Send requested file
+    HttpStatus s = handle_http_request(sock, request);
+    LOG_INFO("client ", ip, " status: ", s, ". Closing connection...");
+  } catch (const std::exception &e) {
+    // NOTE: only `send_all()` throws exception
+    LOG_ERROR("client ", ip, " send_all() exception: ", e.what());
+  }
+}
+
+HttpStatus handle_http_request(Socket &sock, std::string_view request) {
+  if (request.find("Host:") == std::string::npos) {
+    // send_400(); // Bad request
+    return 400;
+  }
+
+  std::string path = "index.html"; // TODO: parse from request
+
+  // Prepare file
+  // NOTE: may add generating HTML document if directory is requested
+  UniqueFd content_fd{open(path.c_str(), O_RDONLY)};
+  if (content_fd == -1) {
+    LOG_ERRNO("open");
+    if (errno == ENOENT) {
+      // send_404(sock); // TODO
+      return 404;
+    }
+    // send_500(sock); // TODO
+    return 500;
+  }
+  // Get file size
+  struct stat st{};
+  if (fstat(content_fd, &st) == -1) {
+    LOG_ERRNO("stat");
+    // send_500(sock); // TODO
+    return 500;
+  }
+  auto content_length = st.st_size;
+
+  // Send headers
+  // const char *mime = get_mime_type(path, path.length());
+  const std::string mime = "text/html"; // TODO: detect by file extension
+  std::stringstream sstr;
+  sstr << "HTTP/1.1 200 OK\r\nContent-Length: " << content_length
+       << "\r\nContent-Type: " << mime << "\r\n\r\n";
+  send_all(sock, sstr.str()); // TODO
+
+#ifdef __linux__
+  ssize_t total_sent = 0;
+  ssize_t sent = 1;
+
+  while (total_sent < content_length) {
+    sent = sendfile(sock, content_fd, nullptr, content_length - total_sent);
+    if (sent == -1) {
+      if (errno == EINTR) {
+        continue;
+      }
+      if (errno == EAGAIN) {
+        LOG_INFO("client timeout (couldn't sendfile)");
+        return 200; // sendfile error, but we sent `200 OK` in headers
+      }
+      LOG_ERRNO("sendfile");
+      return 200;
+    }
+    if (sent == 0) {
+      break;
+    }
+    total_sent += sent;
+  }
+#elif defined __FreeBSD__
+  // NOTE: in FreeBSD loop is required only for non-block I/O
+  if (sendfile(content_fd, sock, 0, static_cast<size_t>(content_length),
+               nullptr, nullptr, 0) == -1) {
+    if (errno == EAGAIN) {
+      LOG_INFO("client timeout (couldn't sendfile)");
+    } else {
+      LOG_ERRNO("sendfile");
+    }
+    return 200; // sendfile error, but we sent `200 OK` in headers
+  }
+#endif
+  return 200;
 }
