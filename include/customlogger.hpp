6e144e516fb2424c47a29008d6c91e49bb56ab80
moriar1 <reflowcion@tuta.io>
2026-05-24 20:01:34 +0500

    fix(logger): remove filenames and line numbers of source file in Release build

--- include/customlogger.hpp
+++ include/customlogger.hpp
@@ -9,17 +9,30 @@ namespace customlogger {
 inline std::mutex glob_log_mut; // locks before writing in `cerr`
 
 template <typename... Args>
-void log_impl(std::string_view level, std::string_view func, int line,
+void log_impl(std::string_view level, std::string_view file, int line,
               Args &&...args) {
   std::lock_guard lk(glob_log_mut);
 
-  std::cerr << "[" << level << "] " << func << ":" << line << ": ";
+  std::cerr << "[" << level << "] " << file << ":" << line << ": ";
   (std::cerr << ... << std::forward<Args>(args)) << '\n'; // fold expression
+}
+
+template <typename... Args>
+void log_release_impl(std::string_view level, Args &&...args) {
+  std::lock_guard lk(glob_log_mut);
+
+  std::cerr << "[" << level << "] ";
+  (std::cerr << ... << std::forward<Args>(args)) << '\n';
 }
 
 } // namespace customlogger
 
 // Use macros instead of inline functions because of __FILE_NAME__ and __LINE__
+
+// Debug macros
+#ifndef NDEBUG
+#define LOG_DEBUG(...)                                                         \
+  customlogger::log_impl("DEBUG", __FILE_NAME__, __LINE__, __VA_ARGS__)
 #define LOG_INFO(...)                                                          \
   customlogger::log_impl("INFO", __FILE_NAME__, __LINE__, __VA_ARGS__)
 #define LOG_ERROR(...)                                                         \
@@ -32,17 +45,29 @@ void log_impl(std::string_view level, std::string_view func, int line,
                            std::system_category().message(_saved_errno));      \
     errno = _saved_errno;                                                      \
   } while (0)
-
 #define LOG_FATAL(...)                                                         \
   do {                                                                         \
     customlogger::log_impl("FATAL", __FILE_NAME__, __LINE__, __VA_ARGS__);     \
     std::abort();                                                              \
   } while (0)
 
-// Debug only macro
-#ifndef NDEBUG
-#define LOG_DEBUG(...)                                                         \
-  customlogger::log_impl("DEBUG", __FILE_NAME__, __LINE__, __VA_ARGS__)
-#else
+#else // Release macros
+
 #define LOG_DEBUG(...) ((void)0)
+#define LOG_INFO(...) customlogger::log_release_impl("INFO", __VA_ARGS__)
+#define LOG_ERROR(...) customlogger::log_release_impl("ERROR", __VA_ARGS__)
+#define LOG_ERRNO(...)                                                         \
+  do {                                                                         \
+    int _saved_errno = errno;                                                  \
+    customlogger::log_release_impl(                                            \
+        "ERROR", __VA_ARGS__, ": ",                                            \
+        std::system_category().message(_saved_errno));                         \
+    errno = _saved_errno;                                                      \
+  } while (0)
+#define LOG_FATAL(...)                                                         \
+  do {                                                                         \
+    customlogger::log_release_impl("FATAL", __VA_ARGS__);                      \
+    std::abort();                                                              \
+  } while (0)
+
 #endif
